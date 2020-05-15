ad48e2f903a434a1d271193bdc5a27160deae385
Srajan Naik <Srajan.Naik@innowise.us>
2020-05-16 04:41:13 +0530

    Initial Commit - learning

---
+++ socialapp.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using socialapp.API.Models;
+
+namespace socialapp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
---
+++ socialapp.API/Models/Value.cs
@@ -0,0 +1,9 @@
+namespace socialapp.API.Models
+{
+    public class Value
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
